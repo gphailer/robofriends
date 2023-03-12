ba957a3616b87f96250161906edde785d1b6b1c9
gphialer <gphailer@outlook.com>
2023-03-11 22:53:32 -0600

    removed error true for testing

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll.js';
+import ErrorBoundry from '../components/ErrorBoundry';
 import '../containers/App.css';
 
 
@@ -36,7 +37,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                <CardList robots={filterdRobots}/>
+                    <ErrorBoundry>
+                        <CardList robots={filterdRobots}/>
+                    </ErrorBoundry>
                 </Scroll> 
             </div>
         );
